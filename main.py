4211f6285e962b461151fa1145b6e3916418d16f
motazayman1324-sudo <motazayman1324@gmail.com>
2026-03-09 01:47:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,33 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 voice_join_times = {}
 
+# ايدي روم التكست
+LOG_CHANNEL_ID = 1473015218211651706
+
+
 @bot.event
 async def on_ready():
     print(f"✅ Logged in as {bot.user}")
 
+
 @bot.event
 async def on_voice_state_update(member, before, after):
 
-    # دخول روم صوتي
+    channel = bot.get_channel(LOG_CHANNEL_ID)
+
+    # دخول فويس
     if before.channel is None and after.channel is not None:
 
         join_time = datetime.now(timezone.utc)
         voice_join_times[member.id] = join_time
 
-        try:
-            await member.send(
-                f"🎙️ Welcome {member.name}! "
-                f"You joined {after.channel.name} at {join_time.strftime('%H:%M')}"
+        if channel:
+            await channel.send(
+                f"🟢 **{member.name}** دخل روم الصوت **{after.channel.name}** "
+                f"الساعة {join_time.strftime('%H:%M')}"
             )
-        except:
-            print(f"Couldn't DM {member.name}")
 
-    # خروج من روم صوتي
+    # خروج فويس
     if before.channel is not None and after.channel is None:
 
         if member.id in voice_join_times:
@@ -42,17 +47,15 @@ async def on_voice_state_update(member, before, after):
 
             minutes, seconds = divmod(int(duration.total_seconds()), 60)
 
-            try:
-                await member.send(
-                    f"👋 {member.name}, you stayed in voice for {minutes}m {seconds}s"
+            if channel:
+                await channel.send(
+                    f"🔴 **{member.name}** خرج من الصوت بعد "
+                    f"{minutes} دقيقة و {seconds} ثانية"
                 )
-            except:
-                print(f"Couldn't DM {member.name}")
 
             del voice_join_times[member.id]
 
 
-# قراءة التوكن من الاستضافة
 TOKEN = os.getenv("TOKEN")
 
 bot.run(TOKEN)
