8c23debd8a5e7895c0edce48ff9011865ff88fc7
motazayman1324-sudo <motazayman1324@gmail.com>
2026-03-09 02:04:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
+import os
 import time
 import json
-import os
 
 TOKEN = os.getenv("TOKEN")
 LOGIN_CHANNEL = 1473015218211651706
@@ -17,7 +17,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 sessions = {}
 points = {}
 
-# تحميل النقاط إذا موجودة
 if os.path.exists("points.json"):
     with open("points.json", "r") as f:
         points = json.load(f)
@@ -28,11 +27,10 @@ def save_points():
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot Online: {bot.user}")
+    print(f"Bot online: {bot.user}")
 
 @bot.event
 async def on_message(message):
-
     if message.author.bot:
         return
 
@@ -41,9 +39,7 @@ async def on_message(message):
 
     member = message.guild.get_member(message.author.id)
 
-    # تسجيل دخول
     if message.content == "تسجيل دخول":
-
         if not member.voice or not member.voice.channel:
             await message.reply("❌ لازم تكون داخل روم صوتي عشان تسجل دخول")
             return
@@ -53,14 +49,11 @@ async def on_message(message):
             return
 
         sessions[str(member.id)] = time.time()
+        await message.reply("✅ تم تسجيل دخولك")
 
-        await message.reply("✅ تم تسجيل دخولك وبدأ حساب الوقت")
-
-    # تسجيل خروج
-    if message.content == "تسجيل خروج":
-
+    elif message.content == "تسجيل خروج":
         if member.voice and member.voice.channel:
-            await message.reply("❌ لازم تطلع من الروم الصوتي قبل تسجيل الخروج")
+            await message.reply("❌ لازم تطلع من الصوتي قبل تسجيل الخروج")
             return
 
         if str(member.id) not in sessions:
@@ -69,7 +62,6 @@ async def on_message(message):
 
         start = sessions[str(member.id)]
         duration = int(time.time() - start)
-
         minutes = duration // 60
 
         del sessions[str(member.id)]
@@ -78,14 +70,10 @@ async def on_message(message):
             points[str(member.id)] = 0
 
         points[str(member.id)] += minutes
-
         save_points()
 
-        await message.reply(
-            f"⏱ مدة حضورك: {minutes} دقيقة\n⭐ نقاطك: {points[str(member.id)]}"
-        )
+        await message.reply(f"⏱ مدة حضورك: {minutes} دقيقة\n⭐ نقاطك: {points[str(member.id)]}")
 
     await bot.process_commands(message)
 
- bot.run(TOKEN)
-
+bot.run(TOKEN)
